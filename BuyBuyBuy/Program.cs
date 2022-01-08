fea69c3e1df2951bb7fc676288aaa119f41d1d4d
CodeShark20xx <codeshark20xx@gmail.com>
2022-01-08 13:04:26 -0500

    First commit

---
+++ BuyBuyBuy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace BuyBuyBuy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                string product = "books";
+                HtmlDocument htmlDocument = web.Load("https://google.com/search?q=buy+" + product);
+                HtmlNodeCollection itemsContainerNodeCollection = htmlDocument.DocumentNode.SelectNodes("//div[@class='top-pla-group-inner']");
+                HtmlNode itemsContainerNode = itemsContainerNodeCollection.First();
+                HtmlNodeCollection itemsNodeCollection = itemsContainerNode.ChildNodes;
+
+                foreach (HtmlNode itemNode in itemsContainerNodeCollection)
+                {
+                    Console.WriteLine(itemNode.InnerText);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
